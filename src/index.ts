127181019dd8cb8d1ede7984810922a507eb1c0d
Jose Luis Yana <joseyananicolas@gmail.com>
2026-01-25 16:52:00 -0500

    feat: ajustes

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,22 @@
-import * as newrelic from 'newrelic';
+import 'newrelic';
 import express from 'express';
-import helmet from 'helmet';
-import hpp from 'hpp';
 import cors from 'cors';
 
 //import { getSafeSwaggerDoc } from '@/config/swagger';
-
 import { envs } from '@/config/envs';
 import { connectRedis } from '@/config/redis';
 import { globalErrorHandler } from '@/utils/errorHandler';
 import { AppError } from '@/utils/AppError';
 import logger from '@/config/logger';
 import { corsOptions } from '@/config/cors';
-import { limiter } from '@/config/rateLimit';
 import routes from './routes';
 
 import prisma from './config/prisma';
 
 const app = express();
 
-// 1. SEGURIDAD INICIAL: Helmet y CORS primero
-app.use(helmet());
 app.use(cors(corsOptions));
-
-// 2. RATE LIMITER: Protege la API antes de gastar recursos procesando el JSON
-if (envs.isProd) {
-  app.use('/api', limiter); 
-}
-
-// 3. PARSERS: Ahora que sabemos que la petición es segura, leemos el cuerpo
-app.use(express.json({ limit: '10kb' }));
-
-// 4. PARAMETER POLLUTION: Limpiamos los query strings
-app.use(hpp({
-  whitelist: ['category']
-}));
+app.use(express.json());
 
 /*app.use(
   '/docs',
@@ -45,10 +27,7 @@ app.use(hpp({
   })
 );*/
 
-// 5. RUTAS: Una sola vez y después de los filtros de seguridad
 app.use('/api', routes);
-
-// Ruta de salud (usa nuestra utilidad envs para consistencia)
 app.get('/health', (req, res) => {
   res.json({ 
     status: 'up', 
