bc20cc02d355e7e76ae3ba3779b4aecdfce357b6
Jose Luis Yana <joseyananicolas@gmail.com>
2026-03-12 12:16:02 -0500

    ajustes

--- src/module/customer/productBranchMovement/productBranchMovement.routes.ts
+++ src/module/customer/productBranchMovement/productBranchMovement.routes.ts
@@ -6,6 +6,7 @@ const router = Router();
 router.get('/', ProductBranchMovementController.getAll);
 router.get('/:id', ProductBranchMovementController.getById);
 router.post('/', ProductBranchMovementController.create);
+router.post('/bulk', ProductBranchMovementController.createBulk);
 router.put('/:id', ProductBranchMovementController.update);
 router.delete('/:id', ProductBranchMovementController.delete);
 
